4a884ba8670ec5021fe8cb28c0abe62bf17444d4
Pakin Kaewsawang <me@pakin.me>
2018-02-05 14:49:03 +0700

    Add fallback image  for HTTPS

--- main.js
+++ main.js
@@ -34,11 +34,11 @@ $(document).ready(function() {
     img.show();
     if (location.protocol === 'https:') {
       if (data.cache == "20") {
-        img.attr("src", "https://images.weserv.nl/?url=" + data.img + "?ct=" + time20);
+        img.attr("src", "https://images.weserv.nl/?url=" + data.img + "?ct=" + time20 + "&errorredirect=ssl:cdn.pakin.me/storage/images/rainradar-error.png");
       } else if (data.imgani == "yes") {
         img.attr("src", "");
       } else {
-        img.attr("src", "https://images.weserv.nl/?url=ssl:cdn.pakin.me/storage/cache/radarimg/8d0ae/" + time5 + "/" + data.img);
+        img.attr("src", "https://images.weserv.nl/?url=ssl:cdn.pakin.me/storage/cache/radarimg/8d0ae/" + time5 + "/" + data.img + "&errorredirect=ssl:cdn.pakin.me/storage/images/rainradar-error.png");
       }
     } else {
       img.attr("src", data.imgprefix + data.img + "?ct=" + time5);
